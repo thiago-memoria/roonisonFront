7f0db7632eb3d562b4a2d6f053d73a43e9b466dd
Thiago Memória Barroso <71512175+thiago-memoria@users.noreply.github.com>
2022-12-11 20:38:11 -0300

    Add files via upload

---
+++ src/components/App/App.tsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2'
+import './App.css';
+import Header from '../Header';
+import Container from '../../shared/Container';
+import Table, { TableHeader } from '../../shared/Table';
+import { Product } from '../../shared/Table/Table.mockdata';
+import ProductForm, { ProductCreator } from '../Products/ProductForm';
+import {
+  getAllProducts,
+  createSingleProduct,
+  updateSingleProduct,
+  deleteSingleProduct
+} from '../../services/Products.service';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Produto' },
+  { key: 'price', value: 'Preço', right: true },
+  { key: 'stock', value: 'Estoque', right: true }
+]
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([])
+  const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(undefined)
+
+  async function fetchData() {
+    const _products = await getAllProducts()
+    setProducts(_products)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  const handleProductSubmit = async (product: ProductCreator) => {
+    try {
+      await createSingleProduct(product)
+      fetchData()
+    } catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
+  }
+
+  const handleProductUpdate = async (newProduct: Product) => {
+    try {
+      await updateSingleProduct(newProduct)
+      setUpdatingProduct(undefined)
+      fetchData()
+    } catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
+  }
+
+  const deleteProduct = async (id: string) => {
+    try {
+      await deleteSingleProduct(id)
+      fetchData()
+      Swal.fire('Uhul!', 'Product successfully deleted', 'success')
+    } catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
+  }
+
+  const handleProductDelete = (product: Product) => {
+    Swal
+      .fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#09f',
+        cancelButtonColor: '#d33',
+        confirmButtonText: `Yes, delete ${product.name}!`
+      })
+      .then((result) => {
+        if (result.value) {
+          deleteProduct(product._id)
+        }
+      })
+  }
+
+  const handleProductDetail = (product: Product) => {
+    Swal.fire(
+      'Product details',
+      `${product.name} costs $${product.price} and we have ${product.stock} available in stock.`,
+      'info'
+    )
+  }
+
+  const handleProductEdit = (product: Product) => {
+    setUpdatingProduct(product)
+  }
+
+  return (
+    <div className="App">
+      <Header title="Estoque" />
+      <Container>
+        <Table
+          headers={headers}
+          data={products}
+          enableActions
+          onDelete={handleProductDelete}
+          onDetail={handleProductDetail}
+          onEdit={handleProductEdit}
+        />
+
+        <ProductForm
+          form={updatingProduct}
+          onSubmit={handleProductSubmit}
+          onUpdate={handleProductUpdate}
+        />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
